3cbe3f573856f54f14c04de28ada1bc4878e35e5
acevnn <ninoacev@gmail.com>
2025-05-19 20:18:13 +0200

    remove drawer for test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 import { getCategories } from "@/graphql/queries";
 import { useEffect, useState } from "react";
-import useBreakpoints from "@/utils/grid";
-import {
-  Bars3BottomLeftIcon,
-  ShoppingCartIcon,
-} from "@heroicons/react/24/solid";
-import { Drawer } from "@/components/Drawer/Drawer";
+// import useBreakpoints from "@/utils/grid";
+// import { Bars3BottomLeftIcon} from "@heroicons/react/24/solid";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+// import { Drawer } from "@/components/Drawer/Drawer";
 import classes from "./Header.module.scss";
 import { useCartStore } from "@/store/cartStore";
 import CartOverlay from "@/components/CartOverlay/CartOverlay";
@@ -20,8 +18,8 @@ export default function Header() {
     { id: "4", name: "tech" },
   ];
   const [categories, setCategories] = useState<Category[]>(fallbackCategories);
-  const [isDrawer, setIsDrawer] = useState(false);
-  const { isMobile, isDesktop } = useBreakpoints();
+  // const [isDrawer, setIsDrawer] = useState(false);
+  // const { isDesktop } = useBreakpoints();
   const totalItems = useCartStore((state) => state.totalCount());
   const isOverlayOpen = useCartStore((state) => state.isOverlayOpen);
   const setOverlayOpen = useCartStore((state) => state.setOverlayOpen);
@@ -58,7 +56,7 @@ export default function Header() {
     fetchCategories();
   }, []);
 
-  const toggleDrawer = () => setIsDrawer((prev) => !prev);
+  // const toggleDrawer = () => setIsDrawer((prev) => !prev);
 
   function toggleCart() {
     setOverlayOpen(!isOverlayOpen);
@@ -66,63 +64,62 @@ export default function Header() {
 
   return (
     <header className={classes.header}>
-      {isDesktop && (
-        <nav>
-          <ul className={classes["header__nav-list"]}>
-            {categories.map((category) => (
-              <li
-                key={category.id}
-                className={`${classes["header__category-link"]} ${
-                  currentCategory === category.name.toLowerCase()
-                    ? classes["header__category-link--active"]
-                    : ""
-                }`}
+      {/*{isDesktop && (*/}
+      <nav>
+        <ul className={classes["header__nav-list"]}>
+          {categories.map((category) => (
+            <li
+              key={category.id}
+              className={`${classes["header__category-link"]} ${
+                currentCategory === category.name.toLowerCase()
+                  ? classes["header__category-link--active"]
+                  : ""
+              }`}
+            >
+              <Link
+                to={
+                  category.name.toLowerCase() === "all"
+                    ? "/all"
+                    : category.name.toLowerCase() === "clothes"
+                      ? "/clothes"
+                      : category.name.toLowerCase() === "shoes"
+                        ? "/shoes"
+                        : "/tech"
+                }
+                onClick={() =>
+                  sessionStorage.setItem(
+                    "activeCategory",
+                    category.name.toLowerCase(),
+                  )
+                }
+                data-testid={
+                  currentCategory === category.name
+                    ? "active-category-link"
+                    : "category-link"
+                }
               >
-                <Link
-                  to={
-                    category.name.toLowerCase() === "all"
-                      ? "/all"
-                      : category.name.toLowerCase() === "clothes"
-                        ? "/clothes"
-                        : category.name.toLowerCase() === "shoes"
-                          ? "/shoes"
-                          : "/tech"
-                  }
-                  onClick={() =>
-                    sessionStorage.setItem(
-                      "activeCategory",
-                      category.name.toLowerCase(),
-                    )
-                  }
-                  data-testid={
-                    currentCategory === category.name
-                      ? "active-category-link"
-                      : "category-link"
-                  }
-                >
-                  {category.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
-
-      {isMobile && (
-        <>
-          <button
-            onClick={toggleDrawer}
-            className={classes["header__hamburger-menu"]}
-          >
-            <Bars3BottomLeftIcon />
-          </button>
-          <Drawer
-            isOpen={isDrawer}
-            toggleDrawer={toggleDrawer}
-            navLinks={categories}
-          />
-        </>
-      )}
+                {category.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      ){/*// }*/}
+      {/*{isMobile && (*/}
+      {/*  <>*/}
+      {/*    <button*/}
+      {/*      onClick={toggleDrawer}*/}
+      {/*      className={classes["header__hamburger-menu"]}*/}
+      {/*    >*/}
+      {/*      <Bars3BottomLeftIcon />*/}
+      {/*    </button>*/}
+      {/*    <Drawer*/}
+      {/*      isOpen={isDrawer}*/}
+      {/*      toggleDrawer={toggleDrawer}*/}
+      {/*      navLinks={categories}*/}
+      {/*    />*/}
+      {/*  </>*/}
+      {/*)}*/}
       <button
         className={classes["header__cart"]}
         data-testid="cart-btn"
