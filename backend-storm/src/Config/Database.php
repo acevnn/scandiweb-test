a94156f89d212b2c85d996ac96ffba65fd7362f5
acevnn <ninoacev@gmail.com>
2025-05-21 18:58:05 +0200

    update database from clever cloud

--- backend-storm/src/Config/Database.php
+++ backend-storm/src/Config/Database.php
@@ -7,24 +7,30 @@ use PDOException;
 
 class Database
 {
-    private PDO $conn;
+    private PDO $connection;
 
     public function __construct()
     {
+        $host = getenv('DB_HOST');
+        $port = getenv('DB_PORT');
+        $db   = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+
         try {
-            $this->conn = new PDO(
-                'mysql:host=fdb1028.awardspace.net;dbname=4636572_scandi',
-                '4636572_scandi',
-                '123Database456'
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection = new PDO($dsn, $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
         } catch (PDOException $e) {
-            die('DB Connection failed: ' . $e->getMessage());
+            die("DB Connection failed: " . $e->getMessage());
         }
     }
 
     public function getConnection(): PDO
     {
-        return $this->conn;
+        return $this->connection;
     }
 }
