c5b5296185e1d7ea35c6120cecf5136e22f6ce04
acevnn <ninoacev@gmail.com>
2025-05-24 14:01:54 +0200

    feature/add-responsivness - queris added

--- backend-storm/src/Config/Database.php
+++ backend-storm/src/Config/Database.php
@@ -34,3 +34,29 @@ class Database
         return $this->connection;
     }
 }
+
+
+//namespace App\Config;
+//
+//use PDO;
+//use PDOException;
+//
+//class Database
+//{
+//    private PDO $conn;
+//
+//    public function __construct()
+//    {
+//        try {
+//            $this->conn = new PDO('mysql:host=localhost;dbname=scandiweb_store', 'root', 'AdminRoot123');
+//            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//        } catch (PDOException $e) {
+//            die('DB Connection failed: ' . $e->getMessage());
+//        }
+//    }
+//
+//    public function getConnection(): PDO
+//    {
+//        return $this->conn;
+//    }
+//}
